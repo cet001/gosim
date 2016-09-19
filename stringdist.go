ced88460ffa6ca452f6d474d3f95e7f0d273954c
Eric Takahashi <etakahashi@finchcomputing.com>
2016-09-18 21:17:50 -0700

    - Added JaroWinklerDist string similarity function.

---
+++ stringdist.go
@@ -0,0 +1,95 @@
+package gosim
+
+// Calculates the Jaro-Winkler distance score.
+type JaroWinkerDist struct {
+	s1matches   []bool
+	s2matches   []bool
+	prefixScale float64
+}
+
+func NewJaroWinklerDist() *JaroWinkerDist {
+	maxStringLen := 1024 * 4
+	return &JaroWinkerDist{
+		s1matches:   make([]bool, maxStringLen),
+		s2matches:   make([]bool, maxStringLen),
+		prefixScale: 0.1,
+	}
+}
+
+// WARNING: This call is NOT threadsafe!
+func (me *JaroWinkerDist) Calc(s1, s2 []byte) float64 {
+	lenS1, lenS2 := len(s1), len(s2)
+	maxMatchDist := (max(lenS1, lenS2) / 2) - 1
+	s1matches, s2matches := me.s1matches, me.s2matches
+
+	// Clear the working space
+	for i := 0; i < max(lenS1, lenS2); i++ {
+		s1matches[i] = false
+		s2matches[i] = false
+	}
+
+	// Count the matches and track which characters from each string matched.
+	m := float64(0)
+	for i, ch1 := range s1 {
+		left, right := max(0, i-maxMatchDist), min(lenS2, i+maxMatchDist+1)
+		for j := left; j < right; j++ {
+			ch2 := s2[j]
+			if ch1 == ch2 && !s2matches[j] {
+				m++
+				s1matches[i] = true
+				s2matches[j] = true
+			}
+		}
+	}
+
+	if m == 0 { // no matches
+		return 0.0
+	}
+
+	// Calculate the number of full transpositions
+	halfTranspositions := 0
+	j := 0
+	for i := 0; i < lenS1; i++ {
+		if s1matches[i] {
+			for !s2matches[j] {
+				j++
+			}
+			if s1[i] != s2[j] {
+				halfTranspositions++
+			}
+			j++
+		}
+	}
+	t := float64(halfTranspositions / 2)
+
+	jaroDist := ((m / float64(lenS1)) + (m / float64(lenS2)) + ((m - t) / m)) / 3.0
+
+	// Calculate the length of the largest common prefix of s1 and s2
+	p := 0
+	for i := 0; i < lenS1; i++ {
+		if i < lenS2 {
+			if s1[i] != s2[i] {
+				break
+			}
+			p++
+		}
+	}
+	jaroWinklerDist := jaroDist + float64(p)*me.prefixScale*(1.0-jaroDist)
+	return jaroWinklerDist
+}
+
+// Returns the lesser of a and b.
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// Returns the greater of a and b.
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ stringdist_test.go
@@ -0,0 +1,60 @@
+package gosim
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestJaroWinklerDist_Calc(t *testing.T) {
+	dist := NewJaroWinklerDist()
+
+	assert.Equal(t, 0.0, dist.Calc([]byte(""), []byte("")))
+	assert.Equal(t, 0.0, dist.Calc([]byte("abc"), []byte("")))
+	assert.Equal(t, 0.0, dist.Calc([]byte(""), []byte("abc")))
+	assert.Equal(t, 0.0, dist.Calc([]byte("abc"), []byte("xyz")))
+
+	assert.Equal(t, 1.0, dist.Calc([]byte("abc"), []byte("abc")))
+
+	// See examples 1 and 2 from https://en.wikipedia.org/wiki/Jaro%E2%80%93Winkler_distance
+	assert.Equal(t, 0.9611111111111111, dist.Calc([]byte("martha"), []byte("marhta")))
+	assert.Equal(t, 0.8133333333333332, dist.Calc([]byte("dixon"), []byte("dicksonx")))
+
+	// See examples from http://alias-i.com/lingpipe/docs/api/com/aliasi/spell/JaroWinklerDistance.html
+	assert.Equal(t, 0.8323809523809523, dist.Calc([]byte("jones"), []byte("johnson")))
+}
+
+func Benchmark_JaroWinklerDist_Calc(b *testing.B) {
+	dist := NewJaroWinklerDist()
+
+	s1values := [][]byte{
+		[]byte("martha"),
+		[]byte("dixon"),
+		[]byte("apple"),
+		[]byte("constitution"),
+		[]byte("mississippi"),
+	}
+
+	s2values := [][]byte{
+		[]byte("marhta"),
+		[]byte("dicksonx"),
+		[]byte("microsoft"),
+		[]byte("intervention"),
+		[]byte("misanthrope"),
+	}
+
+	numValues := len(s1values)
+	i := 0
+
+	calcDist := func() {
+		dist.Calc(s1values[i], s2values[i])
+
+		i++
+		if i == numValues {
+			i = 0
+		}
+	}
+
+	for n := 0; n < b.N; n++ {
+		calcDist()
+	}
+}
